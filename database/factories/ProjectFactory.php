8f6f09d1ade1a75b9eadc2c824ae6dc80c003d3f
mihaimdm <mihai.mdm2007@gmail.com>
2021-01-10 20:34:06 +0200

    project model, migration, factory and seeder

---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Project;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProjectFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Project::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->company,
+            'description' => $this->faker->paragraph,
+            'start_date' => $this->faker->date,
+            'duration' => $this->faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 10),
+        ];
+    }
+}
--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,10 +2,10 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Project;
 use Illuminate\Database\Seeder;
 
-class UserSeeder extends Seeder
+class ProjectSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -14,6 +14,6 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        User::factory(10)->create();
+        Project::factory(20)->create();
     }
 }
