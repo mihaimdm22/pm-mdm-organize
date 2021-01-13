9a719902fd06bc1998ce36867f6919b97a4d27fa
mihaimdm <mihai.mdm2007@gmail.com>
2021-01-13 19:08:59 +0200

    update roles management

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -31,7 +31,7 @@ class RoleController extends Controller
      */
     public function index(Request $request)
     {
-        $roles = Role::orderBy('id', 'DESC')->paginate(5);
+        $roles = Role::orderBy('updated_at', 'DESC')->paginate(5);
         return view('roles.index', compact('roles'))
             ->with('i', ($request->input('page', 1) - 1) * 5);
     }
--- resources/views/roles/index.blade.php
+++ resources/views/roles/index.blade.php
@@ -1,51 +1,99 @@
-@extends('layouts.app')
+@extends('layouts.admin')
 
 @section('content')
-<div class="row">
-    <div class="col-lg-12 margin-tb">
-        <div class="pull-left">
-            <h2>Role Management</h2>
-        </div>
-        <div class="pull-right">
-            {{-- @can('role-create') --}}
-            <a class="btn btn-success" href="{{ route('roles.create') }}"> Create New Role</a>
-            {{-- @endcan --}}
+<!-- Page title & actions -->
+<div class="border-b border-gray-200 px-4 py-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8">
+    <div class="flex-1 min-w-0">
+        <h1 class="text-lg font-medium leading-6 text-gray-900 sm:truncate">
+            Manage Roles
+        </h1>
+    </div>
+    <div class="mt-4 flex sm:mt-0 sm:ml-4">
+        <a href="{{ route('roles.create') }}"
+            class="order-0 inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 sm:order-1 sm:ml-3">
+            Create
+        </a>
+    </div>
+</div>
+
+<!-- Projects table (small breakpoint and up) -->
+<div class="flex flex-col m-3">
+    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                <table class="min-w-full divide-y divide-gray-200">
+                    <thead class="bg-gray-50">
+                        <tr>
+                            <th scope="col"
+                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                Name
+                            </th>
+                            <th scope="col"
+                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                Updated
+                            </th>
+                            <th scope="col" class="relative px-6 py-3">
+                                <span class="sr-only">Actions</span>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody class="bg-white divide-y divide-gray-200">
+
+                        @forelse ($roles as $key => $role)
+                        <tr>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                <div class="flex items-center">
+                                    <div class="ml-4">
+                                        <div class="text-sm font-medium text-gray-900">
+                                            {{ $role->name }}
+                                        </div>
+                                    </div>
+                                </div>
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                {{ $role->updated_at->diffForHumans() }}
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                <a href="{{ route('roles.show',$role->id) }}"
+                                    class="text-green-600 hover:text-green-900">View</a>
+                                <a href="{{ route('roles.edit',$role->id) }}"
+                                    class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                                <form class="inline" action="{{ route('roles.destroy', $role->id) }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="text-red-600 hover:text-red-900" type="submit" title="delete">
+                                        Delete
+                                    </button>
+                                </form>
+                            </td>
+                        </tr>
+                        @empty
+                        <tr>
+                            <td class="px-6 py-4 whitespace-nowrap" colspan="100%">
+                                <div class="text-center text-sm font-medium text-gray-900">
+                                    No roles found!
+                                </div>
+                            </td>
+                        </tr>
+                        @endforelse
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </div>
 
-@if ($message = Session::get('success'))
-<div class="alert alert-success">
-    <p>{{ $message }}</p>
+@if ($roles->hasPages())
+<div class="flex flex-col m-3">
+    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                <div class="min-w-full divide-y divide-gray-200 p-4">
+                    {{ $roles->links() }}
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endif
-
-<table class="table table-bordered">
-    <tr>
-        <th>No</th>
-        <th>Name</th>
-        <th width="280px">Action</th>
-    </tr>
-    @foreach ($roles as $key => $role)
-    <tr>
-        <td>{{ $role->id }}</td>
-        <td>{{ $role->name }}</td>
-        <td>
-            <a class="btn btn-info" href="{{ route('roles.show',$role->id) }}">Show</a>
-            {{-- @can('role-edit') --}}
-            <a class="btn btn-primary" href="{{ route('roles.edit',$role->id) }}">Edit</a>
-            {{-- @endcan --}}
-            <form action="{{ route('roles.destroy',$role->id) }}" method="POST" style="display:inline;">
-                @csrf
-                @method('DELETE')
-                {{-- @can('project-delete') --}}
-                <button type="submit" class="btn btn-danger">Delete</button>
-                {{-- @endcan --}}
-            </form>
-        </td>
-    </tr>
-    @endforeach
-</table>
-
-{!! $roles->render() !!}
 @endsection
